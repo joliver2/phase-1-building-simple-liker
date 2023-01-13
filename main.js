084328c9367f3164c186eb50fbdbeb383f64f18c
joliver427@icloud.com <joliver427@icloud.com>
2023-01-12 20:27:10 -0700

    commit tree cecb0a380f7f0d67ac25f5b96ec6d3eeb41816b7

--- main.js
+++ main.js
@@ -2,7 +2,44 @@
 const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
-// Your JavaScript code goes here!
+let likes = document.querySelectorAll('.like-glyph')
+let counter = 0
+// const counterDisplay = document.createElement('button')
+// const liker = document.querySelector(".like")
+// liker.appendChild(counterDisplay)
+// counterDisplay.textContent = counter
+
+for (heart of likes) {
+  heart.addEventListener('click', addLike)
+  // liker.appendChild(counterDisplay)
+}
+
+function addLike(event) {
+  targetHeart = event.target;
+  // liker.appendChild(counterDisplay)
+  mimicServerCall()
+    .then(function (response) {
+      if (targetHeart.textContent === EMPTY_HEART) {
+        // counter++
+        targetHeart.classList.add('activated-heart')
+        targetHeart.textContent = FULL_HEART;
+        
+        // counterDisplay.textContent = counter
+
+      }
+      else if (targetHeart.textContent === FULL_HEART) {
+        targetHeart.classList.remove('activated-heart')
+        targetHeart.textContent = EMPTY_HEART
+      }
+    })
+    .catch(function (error) {
+      document.querySelector('#modal').classList.remove('hidden')
+      setTimeout(function () {
+        document.querySelector('#modal').classList.add('hidden')
+      }, 3000)
+    });
+}
+
 
 
 
